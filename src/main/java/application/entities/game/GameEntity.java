8e818a7e60d45baf1700b4efe8258c90dcb3c0cc
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2017-12-22 18:53:05 +0300

    delete entities

--- src/main/java/application/entities/game/GameEntity.java
+++ src/main/java/application/entities/game/GameEntity.java
@@ -4,20 +4,20 @@ package application.entities.game;
 import javax.persistence.*;
 import java.sql.Timestamp;
 
-@Entity
-@Table(name = "games")
+//@Entity
+//@Table(name = "games")
 public class GameEntity {
 
-    @Id
-    @Column(name = "gameid", unique = true, nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long gameID;
-
-    @Column(name = "fieldsize", nullable = false)
-    private Integer fieldSize;
-
-    @Column(name = "started", nullable = false)
-    private Timestamp started;
+//    @Id
+//    @Column(name = "gameid", unique = true, nullable = false)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long gameID;
+//
+//    @Column(name = "fieldsize", nullable = false)
+//    private Integer fieldSize;
+//
+//    @Column(name = "started", nullable = false)
+//    private Timestamp started;
 
 
 }
