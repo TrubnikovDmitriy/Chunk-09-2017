91d37130d881082894137277ae9fc943d59551fb
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2017-12-22 19:30:57 +0300

    blet

--- src/main/java/application/entities/game/ScoreEntity.java
+++ src/main/java/application/entities/game/ScoreEntity.java
@@ -1,22 +1,22 @@
 package application.entities.game;
 
-import application.models.user.UserSignUp;
-import application.models.user.UserUpdate;
+//import application.models.user.UserSignUp;
+//import application.models.user.UserUpdate;
+//
+//import javax.persistence.*;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "score")
+//@Entity
+//@Table(name = "score")
 public class ScoreEntity {
-
-    @Id
-    @Column(name = "id", unique = true, nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long scoreID;
-
-    @Column(name = "result", nullable = false)
-    private Double result;
-
-    @Column(name = "user_id", nullable = false)
-    private Long userID;
+//
+//    @Id
+//    @Column(name = "id", unique = true, nullable = false)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long scoreID;
+//
+//    @Column(name = "result", nullable = false)
+//    private Double result;
+//
+//    @Column(name = "user_id", nullable = false)
+//    private Long userID;
 }
